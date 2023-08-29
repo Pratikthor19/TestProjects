ed4b8ed083c4fd1de27911ba36632817e5a3910e
Pratikthorat <pratikgthorat92@gmail.com>
2023-08-29 15:00:38 +0530

    added 1 to 5 number code

--- Test/src/com/thorat/Sample.java
+++ Test/src/com/thorat/Sample.java
@@ -1,5 +1,11 @@
 package com.thorat;
 
 public class Sample {
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("value of i is>>" + i);
+		}
+	}
 
 }
