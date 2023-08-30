a82944d616be698e7ef16c6a46bf94da095a3970
Pratikthor19 <pratikgthorat92@gmail.com>
2023-08-30 14:39:51 +0530

    arraylist code added here

---
+++ Test/src/com/thorat/Array.java
@@ -0,0 +1,17 @@
+package com.thorat;
+
+import java.util.ArrayList;
+
+public class Array {
+	public static void main(String[] args) {
+
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(25);
+		al.add(50);
+		al.add(75);
+		al.add(100);
+
+		System.out.println(al);
+	}
+
+}
